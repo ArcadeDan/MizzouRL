d219fa2e53a091ec56ca41ab480fa4faa72fa445
Dan <lordaniel24@gmail.com>
2025-02-02 17:11:36 -0600

    bracket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::{self, main_loop, BError, BTermBuilder, GameState};
+
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut prelude::BTerm) {
+        ctx.print(1, 1, "Hello World");
+    }
+}
+
+fn main() -> BError{
+    let context = BTermBuilder::simple80x50()
+        .with_title("Mizzou Roguelike")
+        .build()?;
+
+    let gs: State = State {};
+    main_loop(context, gs)
 }
