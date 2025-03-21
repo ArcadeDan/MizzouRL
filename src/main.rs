6ba4fe838dd593123b29519afed9a18655e0a7cc
Dan <lordaniel24@gmail.com>
2025-03-21 15:33:00 -0500

    monsters add

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use specs::prelude::*;
 use ecs::component::{Player, Position, Renderable, State, Viewshed};
 use generation::map::{new_map_rooms_and_corridors, Map};
+use specs::prelude::*;
 
 mod ecs;
+mod game;
 mod generation;
 mod ui;
-mod game;
-
 
 fn main() -> bracket_lib::prelude::BError {
     let mut gs = State { ecs: World::new() };
@@ -18,6 +17,25 @@ fn main() -> bracket_lib::prelude::BError {
 
     let map: Map = new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
+
+    for room in map.rooms.iter().skip(1) {
+        let (x, y) = room.center();
+        gs.ecs
+            .create_entity()
+            .with(Position { x, y })
+            .with(Renderable {
+                glyph: bracket_lib::prelude::to_cp437('g'),
+                fg: bracket_lib::prelude::RGB::named(bracket_lib::prelude::RED),
+                bg: bracket_lib::prelude::RGB::named(bracket_lib::prelude::BLACK),
+            })
+            .with(Viewshed {
+                visible_tiles: Vec::new(),
+                range: 8,
+                dirty: true,
+            })
+            .build();
+    }
+
     gs.ecs.insert(map);
 
     let context = bracket_lib::prelude::BTermBuilder::simple80x50()
@@ -28,7 +46,10 @@ fn main() -> bracket_lib::prelude::BError {
 
     gs.ecs
         .create_entity()
-        .with(Position { x: player_x, y: player_y })
+        .with(Position {
+            x: player_x,
+            y: player_y,
+        })
         .with(Renderable {
             glyph: bracket_lib::prelude::to_cp437('@'),
             fg: bracket_lib::prelude::RGB::named(bracket_lib::prelude::YELLOW),
