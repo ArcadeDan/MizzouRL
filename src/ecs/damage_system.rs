5323378358062e5f4f911fa02122ee010a5fec65
Dan <lordaniel24@gmail.com>
2025-04-27 22:38:40 -0500

    death messages

--- src/ecs/damage_system.rs
+++ src/ecs/damage_system.rs
@@ -1,5 +1,8 @@
-use crate::{CombatStats, SufferDamage};
+use crate::{game::gamelog::GameLog, CombatStats, SufferDamage};
+use bracket_lib::prelude::console;
 use specs::prelude::*;
+
+use super::component::{Name, Player};
 
 pub struct DamageSystem {}
 
@@ -23,10 +26,24 @@ impl DamageSystem {
         let mut dead: Vec<Entity> = Vec::new();
         {
             let combat_stats = ecs.read_storage::<CombatStats>();
+            let players = ecs.read_storage::<Player>();
+            let names = ecs.read_storage::<Name>();
             let entities = ecs.entities();
+            let mut log = ecs.write_resource::<GameLog>();
+
             for (entity, stats) in (&entities, &combat_stats).join() {
                 if stats.hp < 1 {
-                    dead.push(entity);
+                    let player = players.get(entity);
+                    match player {
+                        None => {
+                            let victim_name = names.get(entity);
+                            if let Some(victim_name) = victim_name {
+                                log.entries.push(format!("{} is dead.", &victim_name.name));
+                            }
+                            dead.push(entity);
+                        }
+                        Some(_) => { console::log("You are dead")}
+                    }
                 }
             }
         }
